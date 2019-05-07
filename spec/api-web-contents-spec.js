0a7537f2eb7f183ddec16637e8a2e92a0d600321
Alexandre Lachèze <alexandre.lacheze@gmail.com>
2019-05-07 14:18:44 +0200

    Modify tests about zoom persistence to properly simulate cross-origin navigation

--- spec/api-web-contents-spec.js
+++ spec/api-web-contents-spec.js
@@ -8,6 +8,7 @@ const http = require('http')
 const path = require('path')
 const { closeWindow } = require('./window-helpers')
 const { emittedOnce } = require('./events-helpers')
+const { createNetworkSandbox } = require('./network-helper')
 const chai = require('chai')
 const dirtyChai = require('dirty-chai')
 
@@ -704,6 +705,11 @@ describe('webContents module', () => {
     })
 
     it('cannot persist zoom level after navigation with webFrame', (done) => {
+      const protocol = session.defaultSession.protocol
+      const sandbox = createNetworkSandbox(protocol)
+      sandbox.serveFileFromProtocol('ccc', path.join(fixtures, 'pages', 'c.html'))
+      sandbox.serveFileFromProtocol('ddd', path.join(fixtures, 'pages', 'd.html'))
+
       let initialNavigation = true
       const source = `
         const {ipcRenderer, webFrame} = require('electron')
@@ -716,15 +722,16 @@ describe('webContents module', () => {
         } else {
           const zoomLevel = w.webContents.getZoomLevel()
           assert.strictEqual(zoomLevel, 0)
+          sandbox.reset()
           done()
         }
       })
       ipcMain.once('zoom-level-set', (e, zoomLevel) => {
         assert.strictEqual(zoomLevel, 0.6)
-        w.loadFile(path.join(fixtures, 'pages', 'd.html'))
+        w.loadURL('ddd://page')
         initialNavigation = false
       })
-      w.loadFile(path.join(fixtures, 'pages', 'c.html'))
+      w.loadURL('ccc://page')
     })
   })
 
